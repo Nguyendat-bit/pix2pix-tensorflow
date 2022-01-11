8f8ffa469712c55930b290b31cf23b58623a7a23
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2022-01-11 11:18:53 +0700

    first

---
+++ loss_model.py
@@ -0,0 +1,18 @@
+from tensorflow import keras
+import tensorflow as tf 
+# LSGAN with mse or default - binary_crossentropy
+# L1 or L2
+def disc_loss(loss_func, disc_real_output, disc_gen_output):
+    real_loss = loss_func(tf.experimental.numpy.full_like(disc_real_output, 0.9, dtype= tf.float32), disc_real_output)
+    fake_loss = loss_func(tf.experimental.numpy.full_like(disc_gen_output, 0.1, dtype= tf.float32), disc_gen_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+def gen_loss(loss_func, disc_gen_output, gen_output, target, l1 = True, LAMBDA = 100):
+    gan_loss = loss_func(tf.ones_like(disc_gen_output), disc_gen_output)
+
+    if l1: # Mean absolute error
+        l_loss = tf.reduce_mean(tf.abs(target - gen_output))
+    else: # Mean square error
+        l_loss = tf.reduce_sum(tf.pow(gen_output - target, 2)) / (2 * gen_output.shape[0])
+    total_loss = gan_loss + LAMBDA * l_loss
+    return total_loss
