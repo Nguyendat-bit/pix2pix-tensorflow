bbe4a10558d67aba090805281c2662ca035a0fa3
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2022-01-15 20:59:39 +0700

    g_model

--- generator_model_resnet50.py
+++ generator_model_resnet50.py
@@ -5,23 +5,23 @@ from tensorflow.keras.models import Model
 
 def decoder_block(x, y, filters):
     initializer = tf.random_normal_initializer(0., 0.02)
-    x = Conv2DTranspose(filters, (2,2), strides= 2, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2DTranspose(filters, 4, strides= 2, padding= 'same', kernel_initializer= initializer)(x)
     x = Concatenate(axis = 3)([x,y])
-    x = Conv2D(filters, 3, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2D(filters, 4, padding= 'same', kernel_initializer= initializer)(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
-    x = Conv2D(filters, 3, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2D(filters, 4, padding= 'same', kernel_initializer= initializer)(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
     return x
 
-def generator_model(input_shape= (512,512,3)):
+def generator_model(input_shape= (512,512,3), trainable = True):
     inputs = Input(shape = input_shape)
     initializer = tf.random_normal_initializer(0., 0.02)
 
    
     resnet50 = ResNet50(include_top=False, weights="imagenet", input_tensor=inputs)
-    resnet50.trainable = False
+    resnet50.trainable = trainable
     replace_swish_with_relu(resnet50)
     """ Encoder """
     s1 = resnet50.get_layer("input_1").output           ## (512 x 512)
@@ -34,15 +34,18 @@ def generator_model(input_shape= (512,512,3)):
 
     """ Decoder """
     d1 = decoder_block(b1, s4, 512)                     ## (64 x 64)
+    d1 = Dropout(0.3)(d1)
     d2 = decoder_block(d1, s3, 256)                     ## (128 x 128)
+    d2 = Dropout(0.3)(d2)
     d3 = decoder_block(d2, s2, 128)                     ## (256 x 256)
-    d4 = decoder_block(d3, s1, 64)              ## 512
+    d3 = Dropout(0.3)(d3)
+    d4 = decoder_block(d3, s1, 64)          ## 512            ## 512
 
-    outputs = Conv2D(3,3, padding= 'same', activation= 'tanh', kernel_initializer= initializer)(d4)
-    return Model(inputs, outputs, name = 'generator')
+    outputs = Conv2D(3,4, padding= 'same', activation= 'tanh', kernel_initializer= initializer)(d4)
+    return Model(inputs, outputs, name = 'g_model')
 
 if __name__ == '__main__':
     input_shape = (224,224,3)
-    model = generator_model(input_shape)
+    model = generator_model(input_shape, False)
     model.summary()
 
--- generator_model_vgg19.py
+++ generator_model_vgg19.py
@@ -5,24 +5,24 @@ from tensorflow.keras.models import Model
 
 def decoder_block(x, y, filters):
     initializer = tf.random_normal_initializer(0., 0.02)
-    x = Conv2DTranspose(filters, (2,2), strides= 2, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2DTranspose(filters, 4, strides= 2, padding= 'same', kernel_initializer= initializer)(x)
     x = Concatenate(axis = 3)([x,y])
-    x = Conv2D(filters, 3, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2D(filters, 4, padding= 'same', kernel_initializer= initializer)(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
-    x = Conv2D(filters, 3, padding= 'same', kernel_initializer= initializer)(x)
+    x = Conv2D(filters, 4, padding= 'same', kernel_initializer= initializer)(x)
     x = BatchNormalization()(x)
     x = LeakyReLU(0.2)(x)
     return x
 
-def generator_model(input_shape= (512,512,3)):
+def generator_model(input_shape= (512,512,3), trainable = True):
     inputs = Input(shape = input_shape)
     initializer = tf.random_normal_initializer(0., 0.02)
 
     """ Pre-trained MobileNetV2 """
     vgg19 = VGG19(include_top=False, weights="imagenet", input_tensor=inputs)
     replace_swish_with_relu(vgg19)
-    vgg19.trainable = False
+    vgg19.trainable = trainable
     """ Encoder """
     s1 = vgg19.get_layer("block1_conv2").output         ## (512 x 512)
     s2 = vgg19.get_layer("block2_conv2").output         ## (256 x 256)
@@ -34,14 +34,17 @@ def generator_model(input_shape= (512,512,3)):
     
     """ Decoder """
     d1 = decoder_block(b1, s4, 512)                     ## (64 x 64)
+    d1 = Dropout(0.3)(d1)
     d2 = decoder_block(d1, s3, 256)                     ## (128 x 128)
+    d2 = Dropout(0.3)(d2)
     d3 = decoder_block(d2, s2, 128)                     ## (256 x 256)
+    d3 = Dropout(0.3)(d3)
     d4 = decoder_block(d3, s1, 64)          ## 512
 
-    outputs = Conv2D(3,3, padding= 'same', activation= 'tanh', kernel_initializer= initializer)(d4)
-    return Model(inputs, outputs, name = 'generator')
+    outputs = Conv2D(3,4, padding= 'same', activation= 'tanh', kernel_initializer= initializer)(d4)
+    return Model(inputs, outputs, name = 'g_model')
 
 if __name__ == '__main__':
     input_shape = (224,224,3)
-    model = generator_model(input_shape)
+    model = generator_model(input_shape, False)
     model.summary()
